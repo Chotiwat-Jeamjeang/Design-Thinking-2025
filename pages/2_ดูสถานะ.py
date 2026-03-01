07f189d8b6d516fd65aa0b5dfde1f5f5ed447c5d
Coolindy <56873838+Chotiwat-Jeamjeang@users.noreply.github.com>
2026-03-01 14:09:30 +0700

    Update 2_ดูสถานะ.py

--- pages/2_ดูสถานะ.py
+++ pages/2_ดูสถานะ.py
@@ -2,6 +2,29 @@ import streamlit as st
 from booking import get_all_bookings
 from database import get_connection
 from datetime import date
+import requests
+from PIL import Image
+from io import BytesIO
+
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(
+    "https://dsq.up.ac.th/shapen/images/logo-dsq-color.png",
+    headers=headers
+)
+img = Image.open(BytesIO(response.content))
+
+st.image(img, width=885)
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #265e20;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title("📊 ดูสถานะสนาม")
 
