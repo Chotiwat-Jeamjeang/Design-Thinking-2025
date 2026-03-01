a728eb252fb69d4d6c4c7cdccb51b0a87c0a6c33
Coolindy <56873838+Chotiwat-Jeamjeang@users.noreply.github.com>
2026-03-01 14:10:35 +0700

    Update 3_ประวัติ.py

--- pages/3_ประวัติ.py
+++ pages/3_ประวัติ.py
@@ -1,6 +1,28 @@
 import streamlit as st
 from booking import get_bookings_by_user
+import requests
+from PIL import Image
+from io import BytesIO
 
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(
+    "https://dsq.up.ac.th/shapen/images/logo-dsq-color.png",
+    headers=headers
+)
+img = Image.open(BytesIO(response.content))
+
+st.image(img, width=885)
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #265e20;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 st.title("📜 ประวัติการจอง")
 
 data = get_bookings_by_user(st.session_state.user_id)
