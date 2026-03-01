acf4d4fe7a859ded034f30ed5d464a31d95d2c49
Coolindy <56873838+Chotiwat-Jeamjeang@users.noreply.github.com>
2026-03-01 14:11:07 +0700

    Update 5_ดูฐานข้อมูล.py

--- pages/5_ดูฐานข้อมูล.py
+++ pages/5_ดูฐานข้อมูล.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import pandas as pd
 from database import get_connection
+import requests
+from PIL import Image
+from io import BytesIO
+
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(
+    "https://dsq.up.ac.th/shapen/images/logo-dsq-color.png",
+    headers=headers
+)
+img = Image.open(BytesIO(response.content))
+
+st.image(img, width=885)
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #265e20;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title("🗄️ ดูฐานข้อมูล (Admin Only)")
 
