3692e614342a7af590dd0d60df852f1055fea621
Coolindy <56873838+Chotiwat-Jeamjeang@users.noreply.github.com>
2026-03-01 14:10:53 +0700

    Update 4_Admin.py

--- pages/4_Admin.py
+++ pages/4_Admin.py
@@ -1,5 +1,28 @@
 import streamlit as st
 from booking import get_all_bookings
+import requests
+from PIL import Image
+from io import BytesIO
+
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(
+    "https://dsq.up.ac.th/shapen/images/logo-dsq-color.png",
+    headers=headers
+)
+img = Image.open(BytesIO(response.content))
+
+st.image(img, width=885)
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #265e20;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title("⚙️ Admin Panel")
 
